1f455c86669a7594c84b549ced8d3722d58e55fc
Sabinca <kaiser2169@gmail.com>
2020-02-28 11:17:42 +0300

    Часть1 работает

---
+++ app.js
@@ -0,0 +1,23 @@
+const ws=require("ws").Server;
+
+const port=process.env.PORT || 500;
+
+const server=new ws({port},()=>{
+    console.log(`WebSocket server run on ws://localhost:${port}`);
+});
+
+const clients=new Set();
+
+server.on("connection",function(socket){
+    clients.add(socket);
+
+    socket.on("message",function (message){
+        for(let i of clients){
+            i.send(message);
+        }
+    });
+
+    socket.on("close",function(){
+        clients.delete(socket);
+    });
+});
